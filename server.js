39dec373d505436cef4c3c3cd8cb3e1a9906664d
zxkronos <zxkronos@gmail.com>
2021-10-12 11:28:58 -0300

    texto modificado

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => res.send('<h1>hello world con express!</h1>'))
 
 app.listen(3000);
 console.log('Server on port 3000');
