3450af7405d231b40cdea9a881cfe68273f9ed6a
zxkronos <zxkronos@gmail.com>
2021-10-12 11:35:23 -0300

    puerto update

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('<h1>hello world con express!</h1>'))
 
-app.listen(3000);
-console.log('Server on port 3000');
+app.listen(80);
+console.log('Server on port 80');
