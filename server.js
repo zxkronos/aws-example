bfb675eb1efebd6b47d4489909f8a5761ff48dcb
zxkronos <zxkronos@gmail.com>
2021-10-12 11:44:06 -0300

    puerto cambiado

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('<h1>hello world con express!</h1>'))
 
-app.listen(80);
-console.log('Server on port 80');
+app.listen(8080);
+console.log('Server on port 8080');
